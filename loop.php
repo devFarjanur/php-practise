87863cf61482762239e4d8bac6d2b85613e4475d
devFarjanur <farjanur15-2956@diu.edu.bd>
2025-01-29 22:24:05 +0600

    PHP Practise

--- loop.php
+++ loop.php
@@ -94,7 +94,63 @@ echo "<br>" . "Factorial of 15:" . $fac . "<br>";
 echo "<br>";
 
 echo "<br>";
-echo "<br>" . "Q3: Find the factorial of 5.";
+echo "<br>" . "Q4: Sum all values in an array.";
 echo "<br>";
+
+// for loop
+
+$nums = [2, 4, 6, 8, 10, 12, 15, 14, 0, 1, 2];
+$sum = 0;
+
+foreach ($nums as $num)
+    // $sum = $sum + $num;
+    $sum += $num;
+
+echo "<br>" . "Array Sum:" . $sum . "<br>";
+echo "<br>";
+
+// while loop
+
+$nums = [10, 20, 30, 40, 50, 60, 70, 80];
+$sum = 0;
+$i = 0;
+
+while ($i < count($nums)) {
+    // $sum = $sum + $nums[$i];
+    $sum += $nums[$i];
+    $i++;
+}
+
+echo "<br>" . "Array Sum:" . $sum . "<br>";
+echo "<br>";
+
+echo "<br>";
+echo "<br>" . "Q5: Skip even numbers and stop at 15.";
+echo "<br>";
+
+// for loop
+
+$i = 0;
+
+for ($i = 1; $i <= 50; $i++) {
+    if ($i % 2 == 0) {
+        continue;
+    }
+    if ($i > 39) {
+        break;
+    }
+    echo "<br>" . $i . "<br>";
+}
+
+// while loop
+
+$i = 0;
+
+while ($i <= 70) {
+    if ($i % 2 == 0) {
+        continue;
+    }
+    echo "<br>" . $i . "<br>";
+}
 
 ?>
